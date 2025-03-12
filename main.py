e125cfd22a4f25c36a0dcb94f639aa8937ee9d3a
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-03-13 00:14:23 +0530

    Add main application file with FastAPI setup and CORS middleware

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import uvicorn
+import warnings
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from app.modules import ask_db_route
+
+app = FastAPI(docs_url="/")
+
+origins = ["*"]
+
+warnings.filterwarnings("ignore")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def welcome():
+    return "Welcome to Ask Database Service"
+
+app.include_router(ask_db_route.router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=7000, log_level="info", reload=True)
+    print("running")
