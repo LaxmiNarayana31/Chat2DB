a0bfe7457cd5b3a8351e548091228562dbed0935
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-03-25 22:31:38 +0530

    feat: add status_code field to ResponseSchema

--- app/schema/response_schema.py
+++ app/schema/response_schema.py
@@ -3,5 +3,6 @@ from typing import Union,Optional
 
 class ResponseSchema(BaseModel):
     status: bool = True
+    status_code: int
     response: str
     data: Union[Optional[dict], Optional[list], None] = None
